714b3036da641766e1819e65ac14ff574acb3646
Viktorsodd <viktor@objektia.se>
2018-09-17 21:46:38 +0200

    consts

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const request = require('request');
 const { WebClient } = require('@slack/client');
 // Global variables
-
+const tools = require('./config.js');
+const dealbot = require('./dealbot.js')
 var hubkey = tools.hubkey();
 
 

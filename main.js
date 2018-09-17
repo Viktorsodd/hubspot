793e7578a5478b6308d8fa54ca50a16457f58579
Viktorsodd <viktor@objektia.se>
2018-09-17 21:42:01 +0200

    Changed back path

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const request = require('request');
 const { WebClient } = require('@slack/client');
 // Global variables
-var tools = require('config.js');
+var tools = require('./config.js');
 var hubkey = tools.hubkey();
-var dealbot = require('dealbot.js')
+var dealbot = require('./dealbot.js')
 
 
 // Functions

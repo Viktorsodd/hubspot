5cf12ee2536f31ab3aac3c865ca0b3ec3f3a0c7c
Viktorsodd <viktor@objektia.se>
2018-09-17 23:12:04 +0200

    bodyparser

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ const dealbot = require('./dealbot.js')
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
   app.get('/', (req, res) => res.render('pages/index'))
-  app.use(express.json());       // to support JSON-encoded bodies
-  app.use(express.urlencoded()); // to support URL-encoded bodies
+  app.use(bodyParser);       // to support JSON-encoded bodies
+  
   app.post('/action', function(req, res) {
     
   	//dealbot.enhancedMessage();
   	var attachment_id = req.body.attachment_id;
   	var original_message = req.body.original_message;
   	console.log(attachment_id);
-  	console.log(original_message);
-    res.send(attachment_id);
+  	;
+    res.send(attachment_id.value);
 }); 
 
 

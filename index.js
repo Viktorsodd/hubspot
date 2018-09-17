b142e4315078bb6c86bc2d60652adc5b3aafed03
Viktorsodd <viktor@objektia.se>
2018-09-17 22:14:08 +0200

    changed order

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   .get('/start', (req, res) => res.send('hello world'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  

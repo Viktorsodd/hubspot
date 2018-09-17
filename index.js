5c73585473129f076ba9b18c5d860d052ccff998
Viktorsodd <viktor@objektia.se>
2018-09-17 22:34:07 +0200

    Reordered express

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const main = require('./main.js');
 const request = require('request');
 const { WebClient } = require('@slack/client');
 const app = express()
+var bodyParser = require('body-parser');
 
   app.use(express.static(path.join(__dirname, 'public')))
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
   app.get('/', (req, res) => res.render('pages/index'))
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  
-app.post('/api/users', function(req, res) {
+  app.post('/api/users', function(req, res) {
     
 
     res.send('loool');
-});
+}); 
+
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  

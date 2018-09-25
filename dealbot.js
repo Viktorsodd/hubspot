139410dc52cd78562fb9c74580b304e8cd263b47
Viktorsodd <viktor@objektia.se>
2018-09-25 20:18:26 +0200

    Changed method of sending message

--- dealbot.js
+++ dealbot.js
@@ -9,15 +9,22 @@ const webhook = new IncomingWebhook(url);
 
 // Send simple text to the webhook channel
 var message = function sendMessage(content){
-	console.log('sending message')
-	//console.log(content)
-	webhook.send(content, function(err, res) {
-    if (err) {
-        console.log('Error:', err);
-    } else {
-        console.log('Message sent: ', res);
-    }
-});
+		// An access token (from your Slack app or custom integration - xoxa, xoxp, or xoxb)
+const token = process.env.slack_oath;
+
+const web = new WebClient(token);
+
+// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+const conversationId = 'C0PM35ALC';
+
+// See: https://api.slack.com/methods/chat.postMessage
+web.chat.postMessage({ channel: conversationId, text: content })
+  .then((res) => {
+    // `res` contains information about the posted message
+    console.log('Message sent: ', res.ts);
+  })
+  .catch(console.error);
+	
 }
 
 var enhancedMessage = function sendenhancedMessage(){
@@ -27,7 +34,7 @@ const token = process.env.slack_oath;
 const web = new WebClient(token);
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = 'D19F2DLJV';
+const conversationId = 'C0PM35ALC';
 
 // See: https://api.slack.com/methods/chat.postMessage
 web.chat.postMessage({ channel: conversationId, text: 'Hello there' })
